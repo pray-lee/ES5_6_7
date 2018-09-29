468ad641e0c0e6359aae10948a50051623fb0f5a
pray-lee <praycis@gmail.com>
2018-09-29 11:39:03 +0800

    add example

---
+++ 变量提升/example.js
@@ -0,0 +1,27 @@
+// test 1
+// if(!a in window){
+//   var a = 1
+// }
+// console.log(a) //undefined, 因为JS没有块级作用域
+
+//test 2
+// console.log(a) // function a
+// var a = 1
+// console.log(a); // 1
+//
+// function a() {
+//
+// }
+
+//test 3
+// f()
+//   console.log(a); // a is not defined
+// console.log(b); // 9
+// console.log(c); // 9
+//
+// function f() {
+//   var a = b = c = 9
+//   console.log(a); // 9
+//   console.log(b); // 9
+//   console.log(c); // 9
+// }
